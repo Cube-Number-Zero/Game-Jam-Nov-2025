00f931cacf74343c241bc5d9aaf03b2b405e580a
Sophie <cubenumberzero@gmail.com>
2025-11-16 10:40:35 -0500

    Portal decay

--- Scripts/flower.gd
+++ Scripts/flower.gd
@@ -2,6 +2,21 @@ class_name Flower extends Node2D
 
 enum FlowerType {FLOWER_COLOR_1, FLOWER_COLOR_2, FLOWER_COLOR_3}
 
+const FLOWER_TYPES: Array[Flower.FlowerType] = [Flower.FlowerType.FLOWER_COLOR_1, Flower.FlowerType.FLOWER_COLOR_2, Flower.FlowerType.FLOWER_COLOR_3]
+
 var type: FlowerType
 
 var cell: Vector2i
+
+const FLOWER_REMOVAL_CHANCE: float = 0.4 ## The odds this flower will be removed when FlowerRemovalTimer expires, if possible
+
+func check_for_flower_decay() -> void:
+	var connections: int = $"../..".count_flower_connections(cell)
+	if 0 < connections and connections <= 2: # Viable for check for flower decay
+		$"../..".remove_flower(cell)
+
+
+func _on_flower_removal_timer_timeout() -> void:
+	return
+	if randf() <= FLOWER_REMOVAL_CHANCE:
+		check_for_flower_decay()
--- Scripts/portal.gd
+++ Scripts/portal.gd
@@ -5,7 +5,7 @@ var cell: Vector2i
 
 static var all_portal_list: Array[Portal] = [] ## A list of all portals
 
-
+const PORTAL_REMOVAL_CHANCE: float = 0.75 ## The odds this portal will be removed when PortalRemovalTimer expires, if this portal is not being used
 
 func _ready() -> void:
 	all_portal_list.append(self)
@@ -13,9 +13,10 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	#TESTING ONLY!
-	assert($"../../TileMapLayer1".get_cell_atlas_coords(cell) == Board.PORTAL_ATLAS_COORDS)
-	assert($"../../TileMapLayer2".get_cell_atlas_coords(cell) == Board.PORTAL_ATLAS_COORDS)
-	assert($"../../TileMapLayer3".get_cell_atlas_coords(cell) == Board.PORTAL_ATLAS_COORDS)
+	if not is_queued_for_deletion():
+		assert($"../../TileMapLayer1".get_cell_atlas_coords(cell) == Board.PORTAL_ATLAS_COORDS)
+		assert($"../../TileMapLayer2".get_cell_atlas_coords(cell) == Board.PORTAL_ATLAS_COORDS)
+		assert($"../../TileMapLayer3".get_cell_atlas_coords(cell) == Board.PORTAL_ATLAS_COORDS)
 
 
 static func link_portals(portal1: Portal, portal2: Portal) -> void:
@@ -32,3 +33,23 @@ static func update_colors() -> void:
 		portal.get_node(^"Sprite2D").self_modulate = Color.from_hsv(hue, 0.8, 1.0)
 		portal.get_node(^"Label").text = str(j + 1)
 		portal.get_node(^"Label").self_modulate = Color.from_hsv(fmod(hue + 0.5, 1.0), 0.4, 1.0)
+
+func check_for_portal_decay() -> void:
+	if not is_queued_for_deletion():
+		if $"../..".is_portal_connected(cell): return
+		if linked_portal.get_node(^"../..").is_portal_connected(linked_portal.cell): return
+		delete_portal_pair()
+		# No connections! Delete me!
+
+func delete_portal_pair() -> void:
+	$"../..".delete_portal(cell)
+	linked_portal.get_node(^"../..").delete_portal(linked_portal.cell)
+	all_portal_list.erase(self)
+	all_portal_list.erase(linked_portal)
+	self.queue_free()
+	linked_portal.queue_free()
+	update_colors()
+
+func _on_portal_removal_timer_timeout() -> void:
+	if randf() <= PORTAL_REMOVAL_CHANCE:
+		check_for_portal_decay()
